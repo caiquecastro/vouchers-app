45af2799ede0d4770ffbd111821b4bf796896268
Caique de Castro <caiquecastro@outlook.com>
2017-11-12 18:40:11 -0200

    Add pagination to vouchers page

--- resources/views/vouchers/index.blade.php
+++ resources/views/vouchers/index.blade.php
@@ -69,6 +69,8 @@
                         @endforeach
                     </tbody>
                 </table>
+
+                {{ $vouchers->links('pagination::bootstrap-4') }}
             </div>
         </div>
     </div>
