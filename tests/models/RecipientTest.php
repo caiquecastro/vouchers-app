23ce97f5572fa1d79d6ed0c43017d9affab2ebba
Caique de Castro <caiquecastro@outlook.com>
2017-11-12 09:40:12 -0200

    Add test for unique recipient email

--- tests/models/RecipientTest.php
+++ tests/models/RecipientTest.php
@@ -17,4 +17,20 @@ class RecipientTest extends TestCase
         $this->assertEquals('John Doe', $recipient->name);
         $this->assertEquals('johndoe@example.com', $recipient->email);
     }
+
+    public function testRecipientEmailButBeUnique()
+    {
+        \App\Recipient::create([
+            'name' => 'John Doe',
+            'email' => 'johndoe@example.com',
+        ]);
+
+        $this->expectException(\Illuminate\Database\QueryException::class);
+        $this->expectExceptionMessage('Duplicate entry \'johndoe@example.com\' for key \'recipients_email_unique\'');
+
+        \App\Recipient::create([
+            'name' => 'John Doe',
+            'email' => 'johndoe@example.com',
+        ]);
+    }
 }
