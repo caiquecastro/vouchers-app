258b75f4eae6b8cc64fde27fa33d14ec6a85d332
Caique de Castro <caiquecastro@outlook.com>
2018-08-15 19:20:59 -0300

    Add seeder class for recipients table

--- database/seeds/RecipientsTableSeeder.php
+++ database/seeds/RecipientsTableSeeder.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Recipient;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class RecipientsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call('RecipientsTableSeeder');
+        factory(Recipient::class, 100)->create();
     }
 }
