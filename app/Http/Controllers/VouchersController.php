1104ccaf0e11e20b3668a57d9be4cd2f92d05cfd
Caique de Castro <caiquecastro@outlook.com>
2018-08-15 21:06:02 -0300

    Add some notes and some alias for classes

--- app/Http/Controllers/VouchersController.php
+++ app/Http/Controllers/VouchersController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Offer;
 use App\Voucher;
 use Carbon\Carbon;
-use Illuminate\Support\Str;
+use App\Recipient;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 
@@ -13,47 +13,55 @@ class VouchersController extends Controller
 {
     public function index()
     {
-        $totalVouchers = \App\Voucher::count();
-        $usedVouchers = \App\Voucher::where('used_at', '<>', null)->count();
-        $unusedVouchers = \App\Voucher::where('used_at', '=', null)->count();
+        $totalVouchers = Voucher::count();
+        $usedVouchers = Voucher::where('used_at', '<>', null)->count();
+        $unusedVouchers = Voucher::where('used_at', '=', null)->count();
 
-        $vouchers = \App\Voucher::with('offer');
+        $vouchers = Voucher::with('offer');
 
+        // Allow query voucher by its code
         if (request()->has('q')) {
-            $vouchers->where('code', 'LIKE', '%' . request('q') . '%');
+            $vouchers->where('code', request('q'));
         }
 
         $vouchers = $vouchers->paginate();
 
+        // Create view with the paginated vouchers and the stats
         return view('vouchers.index')
             ->with(compact('vouchers', 'totalVouchers', 'usedVouchers', 'unusedVouchers'));
     }
 
     public function create()
     {
-        $offers = \App\Offer::all();
+        $offers = Offer::all();
 
         return view('vouchers.create')->withOffers($offers);
     }
 
     public function store(Request $request)
     {
+        // Validate the request to create voucher
         $this->validate($request, [
             'offer_name' => 'required',
             'offer_discount' => 'required',
             'expires_at' => 'required|date_format:Y-m-d\TH:i',
         ]);
 
+        // Create an offer
         $offer = Offer::create([
             'name' => $request->offer_name,
             'discount' => $request->offer_discount,
         ]);
 
-        $recipients = \App\Recipient::all();
+        $recipients = Recipient::all();
         $expiresAtAttribute = Carbon::parse($request->expires_at);
 
+        // And then for each recipient.. create a voucher for this offer.
+        // If we have a large number of recipients in our database,
+        // the query will be slow...
+        // @TODO: Queue the job to be done in background
         foreach ($recipients as $recipient) {
-            \App\Voucher::create([
+            Voucher::create([
                 'offer_id' => $offer->id,
                 'recipient_id' => $recipient->id,
                 'expires_at' => $expiresAtAttribute,
